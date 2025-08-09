b030cb169307d91f1dda29a4c83fab10cabb5a55
guneettt <83058732+guneettt@users.noreply.github.com>
2025-08-09 12:08:02 -0600

    name page added

--- frontend/mobile/src/navigations/RootNavigator.tsx
+++ frontend/mobile/src/navigations/RootNavigator.tsx
@@ -1,17 +1,20 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomePage from '../screens/WelcomePage';
 import ProtectionScreen from '../screens/ProtectionScreen';
+import NamePage from '../screens/NamePage';
 
 export type RootStackParamList = {
   Welcome: undefined;
   Protection: undefined;
+  Name: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Name">
+      <Stack.Screen name="Name" component={NamePage} />
       <Stack.Screen name="Welcome" component={WelcomePage} />
       <Stack.Screen name="Protection" component={ProtectionScreen} />
     </Stack.Navigator>
