958753b57346a6ff3a2570a8296c3fffd64e5bac
Anthony Mirza <amirza2@ualberta.ca>
2025-08-08 21:00:22 -0600

    Setting up db for phone number login

---
+++ backend/SETUP.md
@@ -0,0 +1,6 @@
+# Setup Instructions:
+
+1. Create and activate Conda enviroment:
+
+conda env create -f environment.yml
+conda activate ./env
---
+++ backend/app/db/connect.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from supabase import create_client, Client
+
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+print(f' the url is:{url}')
+supabase: Client = create_client(url, key)
+
+
+
+response = supabase.table("accounts").select("*").execute()
+print(response)
